4e9ddfd4402645e2a0f31c370c3ecb139a872986
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-05 15:24:56 +0530

    WIP

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -17,18 +17,6 @@ class Interest(models.Model):
     def __str__(self):
         return self.label
 
-
-class UserInterest(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    interest = models.ForeignKey(Interest, on_delete=models.PROTECT)
-
-    class Meta:
-        unique_together = (
-            "user",
-            "interest",
-        )
-
-
 class UserType(models.Model):
     label = models.CharField(max_length=100, unique=True)
 
@@ -40,3 +28,55 @@ class AuthenticationType(models.Model):
 
     def __str__(self):
         return self.label
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.CharField(max_length=100, blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT, blank=False)
+    description = models.TextField(max_length=1000)
+    birth_date = models.DateField()
+    user_type = models.ForeignKey(UserType, on_delete=models.PROTECT)
+    authentication_type = models.ForeignKey(AuthenticationType, on_delete=models.PROTECT)
+    interests = models.ManyToManyField(Interest)
+
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+    
+
+class Degree(models.Model):
+    label = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.label
+    
+class Institution(models.Model):
+    label = models.CharField(max_length=100, unique=True)
+    profile_picture = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.label
+    
+class Education(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, on_delete=models.PROTECT)
+    degree = models.ForeignKey(Degree, on_delete=models.PROTECT)
+    field_of_study = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.user_profile.user.username}'s Education"
+    
+
+class WorkExperience(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    company = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    profile_picture = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user_profile.user.username}'s Work Experience"
