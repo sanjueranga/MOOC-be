d7c0d9a784366f12056a74c2af38de8084cff3e8
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-05 15:10:16 +0530

    feat: intersts model

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -10,3 +10,20 @@ class Country(models.Model):
 
     def __str__(self):
         return self.label
+
+class Interest(models.Model):
+    label = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.label
+
+
+class UserInterest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    interest = models.ForeignKey(Interest, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = (
+            "user",
+            "interest",
+        )
