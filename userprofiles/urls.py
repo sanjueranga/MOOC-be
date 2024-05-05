cf0b1e7472322c0d115b405213dd78e33b086965
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-05 16:26:47 +0530

    feat: test

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserRegistrationAPIView
 
 urlpatterns = [
-    path("register/", UserRegistrationAPIView.as_view(), name="register"),
+    path("register/", UserRegistrationAPIView.as_view(), name="user-registration"),
 ]
