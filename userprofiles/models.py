84d5e5509ce0ba711a4a1e936135ca04a994d0a9
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-05 15:25:38 +0530

    feat: userprofile models are done

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 class Country(models.Model):
     label = models.CharField(max_length=100, unique=True)
-
     class Meta:
         verbose_name_plural = "Countries"
 
