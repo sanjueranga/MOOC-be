6e2f002c6a684203f63909e7026c60752f66657c
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-05-08 23:42:15 +0530

    feat: create course response

--- courses/views.py
+++ courses/views.py
@@ -8,3 +8,13 @@ from .models import Course
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        response = super().create(request, *args, **kwargs)
+        response.data = {
+            "status": "success",
+            "message": "Course created successfully",
+            "data": response.data,
+        }
+        return response
